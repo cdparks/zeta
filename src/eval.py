20c585b7707f6c7277d96a2865b6c1891a7b8637
Christopher Parks <christopher.daniel.parks@gmail.com>
2013-05-04 07:17:31 -0700

    Replace jump table dictionary with Forms object and register decorator

--- src/eval.py
+++ src/eval.py
@@ -11,6 +11,25 @@ from src.parsers import parse, parse_file
 from src.primitives import *
 from src.operators import *
 
+class Forms(object):
+    """Map symbol name to form implementation"""
+    def __init__(self):
+        self.actions = {}
+
+    def register(self, function):
+        """Register form with name"""
+        name = function.__name__.split('_')[-1]
+        self.actions[Symbol(name)] = function
+        return function
+
+    def get(self, name):
+        """Get implementation by name"""
+        return self.actions.get(name)
+
+# Module level jump table for form implementations
+forms = Forms()
+
+@forms.register
 def eval_if(exprs, env):
     """ (if (null? ls) nil (car ls)) """
     test, _ = eval(car(exprs), env)
@@ -19,6 +38,7 @@ def eval_if(exprs, env):
     else:
         return eval(car(cdr(cdr(exprs))), env)
 
+@forms.register
 def eval_let(body, env):
     """ (let ([x 2] [y 8]) (+ x y)) """
     local_env = Environment(scope=env)
@@ -28,6 +48,7 @@ def eval_let(body, env):
     value, _ = eval(single(Symbol('BEGIN')) + cdr(body), local_env)
     return value, env
 
+@forms.register
 def eval_lambda(function, env):
     """
     (lambda (x)(* x x)) => (LAMBDA (X)(BEGIN (* x x)) (ENV))
@@ -43,10 +64,12 @@ def eval_lambda(function, env):
         body = single(Symbol('BEGIN')) + cdr(function)
     return (Symbol('LAMBDA-CLOSURE'), formals, body, env), env
 
+@forms.register
 def eval_quote(ls, env):
     """ (quote (1 2 3 4)) or '(1 2 3 4) """
     return car(ls), env
 
+@forms.register
 def eval_cond(conds, env):
     """
     (cond
@@ -60,6 +83,7 @@ def eval_cond(conds, env):
         conds = cdr(conds)
     return NIL, env
 
+@forms.register
 def eval_begin(expressions, env):
     """
     (begin
@@ -72,6 +96,7 @@ def eval_begin(expressions, env):
         expressions = cdr(expressions)
     return value, env
 
+@forms.register
 def eval_or(expressions, env):
     """Short circuit or"""
     value = False
@@ -83,6 +108,7 @@ def eval_or(expressions, env):
         expressions = cdr(expressions)
     return value, env
 
+@forms.register
 def eval_and(expressions, env):
     """Short circuit and"""
     value = True
@@ -94,6 +120,7 @@ def eval_and(expressions, env):
         expressions = cdr(expressions)
     return value, env
 
+@forms.register
 def eval_define(rest, env):
     """
     Names:
@@ -121,32 +148,19 @@ def eval_define(rest, env):
         env[name] = closure
         return NIL, env
 
+@forms.register
 def eval_delete(rest, env):
     if isatom(car(rest)):
         env.pop(car(rest))
     return NIL, env
 
+@forms.register
 def eval_load(ls, env):
     with open(car(ls)) as stream:
         value = NIL
         for expression in parse_file(stream):
             value, env = eval(expression, env)
         return value, env
-
-# Jump table for forms.
-forms = {
-    Symbol('IF'): eval_if,
-    Symbol('LAMBDA'): eval_lambda,
-    Symbol('LET'): eval_let,
-    Symbol('QUOTE'): eval_quote,
-    Symbol('COND'): eval_cond,
-    Symbol('OR'): eval_or,
-    Symbol('AND'): eval_and,
-    Symbol('BEGIN'): eval_begin,
-    Symbol('DEFINE'): eval_define,
-    Symbol('DELETE'): eval_delete,
-    Symbol('LOAD'): eval_load,
-}
 
 # Some useful selectors
 def closure_body(closure):
@@ -157,7 +171,6 @@ def closure_params(closure):
 
 def closure_env(closure):
     return car(cdr(cdr(cdr(closure))))
-
 
 def eval(ls, env):
     """Evaluate s-expression parsed into nested tuples"""
